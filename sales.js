d715032f6cdf9d43b4960b7de1fd9455c44c5696
Robin Miller <rmillerpdx@gmail.com>
2017-08-08 15:56:45 -0700

    finish table population code

--- sales.js
+++ sales.js
@@ -1,58 +1,66 @@
-var openHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-//create table headers populated with openHours elements
-
 var CookieStore = function (name, minCustomers, maxCustomers, avgCookiesPerCust, elementId){
-  this.name = name;
-  this.minCustomers = minCustomers;
-  this.maxCustomers = maxCustomers;
-  this.avgCookiesPerCust = avgCookiesPerCust;
-  this.elementId = elementId;
-  this.staticCookies = [];
-  this.addToDom();
+    this.name = name;
+    this.minCustomers = minCustomers;
+    this.maxCustomers = maxCustomers;
+    this.avgCookiesPerCust = avgCookiesPerCust;
+    this.elementId = elementId;
+    this.staticCookies = [];
+    this.addToDom();
 };
 
 CookieStore.prototype.randomCust = function () {
-  return (Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers);
+    return (Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers);
 };
 
 CookieStore.prototype.avgCookiesPerHour = function () {
-  return this.randomCust() * this.avgCookiesPerCust;
+    return this.randomCust() * this.avgCookiesPerCust;
 };
 
 CookieStore.prototype.cookieDataArray = function () {
-  for (var i = 0; i < 15; i++){
-    this.staticCookies.push(Math.floor(this.avgCookiesPerHour()));
-  }
+    for (var i = 0; i < 15; i++){
+        this.staticCookies.push(Math.floor(this.avgCookiesPerHour()));
+    }
 };
 
 CookieStore.prototype.addToDom = function () {
-  this.cookieDataArray();
+    this.cookieDataArray();
   
+    //Create TR element with id as this.elementId
+    var container = document.getElementById('masterTable');
+    var createTableRow = document.createElement('tr');
+    createTableRow.id = this.elementId;
+    container.appendChild( createTableRow );
 
-  //Create TR element with id as this.elementId
-  var container = document.getElementById('masterTable');
-  var createTableRow = document.createElement('tr');
-  createTableRow.id = this.elementId;
-  container.appendChild( createTableRow );
-
-  //Create row of TD elements containing location name, cookie data, and total
-  var container = document.getElementById(this.elementId);
-  var createTableElement = document.createElement( 'td' );
-  createTableElement.innerHTML = '<b>' + this.name + '</b>';
-  container.appendChild( createTableElement );
-  for (var i = 0; i < 15; i++) {
+    //Create row of TD elements containing location name, cookie data, and total
+    var container = document.getElementById(this.elementId);
     var createTableElement = document.createElement( 'td' );
-    createTableElement.innerHTML = this.staticCookies[i];
+    createTableElement.innerHTML = '<b>' + this.name + '</b>';
     container.appendChild( createTableElement );
-  }
-  var cookieSum = 0;
-  for (var i = 0; i < this.staticCookies.length; i++){
-    cookieSum = cookieSum + this.staticCookies[i];
-  }
-  createTableElement.innerHTML = ('<b>' + cookieSum + '</b>');
-  container.appendChild( createTableElement );
+    for (var i = 0; i < this.staticCookies.length; i++) {
+        var createTableElement = document.createElement( 'td' );
+        createTableElement.innerHTML = this.staticCookies[i];
+        container.appendChild( createTableElement );
+    }
+    var cookieSum = 0;
+    for (var i = 0; i < this.staticCookies.length; i++){
+        cookieSum = cookieSum + this.staticCookies[i];
+    }
+    var createTableElement = document.createElement( 'td' );
+    createTableElement.innerHTML = ('<b>' + cookieSum + '</b>');
+    container.appendChild( createTableElement );
 
 };
+
+//create table headers populated with openHours elements
+var openHours = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm', 'Total'];
+var tableHeader = document.getElementById('masterTable');
+var createTableHead = document.createElement( 'th' );
+tableHeader.appendChild( createTableHead );
+for (var i = 0; i < openHours.length; i++) {
+    var createTableHead = document.createElement( 'th' );
+    createTableHead.innerHTML = openHours[i];
+    tableHeader.appendChild( createTableHead );
+}
 
 var pdxAirport = new CookieStore('PDX Airport', 23, 65, 6.3, 'pdxairport');
 var pioneerSquare = new CookieStore('Pioneer Square', 3, 24, 1.2, 'pioneersquare');
